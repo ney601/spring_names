7006afd4270e51b04c852051ef5262654412b0b1
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-02-23 07:19:05 +0000

    created a POJO and refactored the controller to use the pojo as well

--- src/main/java/com/example/task_01/controllers/TaskController.java
+++ src/main/java/com/example/task_01/controllers/TaskController.java
@@ -1,3 +1,20 @@
 package com.example.task_01.controllers;
 
-public class TaskController {}
+import com.example.task_01.models.Celebration;
+import com.example.task_01.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class TaskController {
+
+    @GetMapping()
+    public String newGreeting() {
+        Greeting greet = new Greeting("Anna", "Morning");
+        return "Good" + " " + greet.getTimeOfDay() + " " + greet.getName() + "!";
+    }
+
+}
